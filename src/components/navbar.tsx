db2b914fc02e269f04e9075e85eccac55fd8b97b
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-02 23:55:42 -0400

    fix a lot

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,103 +1,117 @@
-import { Link, useLocation } from "@tanstack/react-router"
-import { CoffeeIcon } from "lucide-react"
-import { ThemeToggle } from "./themes/theme-toggle"
-import { DASHBOARD_LINKS, HOME_LINKS } from "@/constants"
-import { CartDropdown } from "@/components/cart"
+import { Link, useLocation } from "@tanstack/react-router";
+import { CoffeeIcon } from "lucide-react";
+import { ThemeToggle } from "./themes/theme-toggle";
+import { LinkItem } from "@/constants";
+import { CartDropdown } from "@/components/cart";
+import { useAuth } from "@/context/auth";
 // shadcn
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "@/components/ui/avatar"
-import { cn } from "@/lib/utils"
-import { useAuth } from "@/context/auth"
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
 
+type Props = {
+    links: LinkItem[]
+}
 
-export function Navbar() {
-    const pathname = useLocation({ select: (location) => location.pathname })
-    const LINKS = pathname.includes("/dashboard")
-        ? DASHBOARD_LINKS
-        : HOME_LINKS
+export function Navbar({ links }: Props) {
+    const pathname = useLocation({ select: (location) => location.pathname });
+    const { user } = useAuth()
 
-    return <div className="px-4 py-2 md:py-4 md:px-10">
-        <header className="flex justify-between items-center max-w-screen-xl mx-auto">
-            <div className="flex gap-2 items-center">
+    return (
+        <div className="px-4 py-2 md:px-10 md:py-4">
+            <header className="mx-auto flex max-w-screen-xl items-center justify-between">
                 <Link to="/" className="group">
-                    <CoffeeIcon size={25} className="group-hover:-rotate-12 transition-transform" />
+                    <CoffeeIcon className="h-6 w-6 transition-transform group-hover:-rotate-12" />
                 </Link>
                 <nav className="hidden sm:block">
-                    {LINKS.map(link =>
-                        <Link to={link.href} key={link.name}>
-                            <Button
-                                variant="link"
-                                size="sm"
-                                className={cn("dark:text-white/60",
-                                    { "underline": pathname === link.href }
-                                )}
-                            >
-                                {link.name}
-                            </Button>
-                        </Link>
-                    )}
+                    {links.map((link) => {
+                        console.log(link)
+                        if (!link.role || link.role === user?.role)
+                            return <Link to={link.url} key={link.label}>
+                                <Button
+                                    variant="link"
+                                    size="sm"
+                                    className={cn("dark:text-white/70", {
+                                        underline: pathname === link.url,
+                                    })}
+                                >
+                                    <link.icon className="mr-2 h-4 w-4" />
+                                    <p className="hidden md:block">
+                                        {link.label}
+                                    </p>
+                                </Button>
+                            </Link>
+                    })}
                 </nav>
-            </div>
-            <div className="flex items-center gap-2">
-                <CartDropdown />
-                <ThemeToggle />
-                <UserDropdown />
-            </div>
-        </header >
-    </div >
+                <div className="flex items-center gap-2">
+                    <CartDropdown />
+                    <ThemeToggle />
+                    <UserDropdown />
+                </div>
+            </header>
+        </div>
+    );
 }
-
 
 function UserDropdown() {
-    const { isAuthenticated, user, logout } = useAuth()
+    const { isAuthenticated, user, logout } = useAuth();
 
-    return <DropdownMenu>
-        <DropdownMenuTrigger>
-            <Avatar className="w-8 h-8 border p-1 hover:border-primary">
-                <AvatarImage src="src/assets/react.svg" alt="user" />
-                <AvatarFallback>{user?.username}</AvatarFallback>
-            </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuLabel> {user?.username} </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-                <Button size="sm" variant="ghost" className="flex mb-2 justify-start w-full rounded-[0.5rem]">
-                    <Link to="/"> Perfil </Link>
-                </Button>
-            </DropdownMenuItem>
-            {isAuthenticated
-                ? <DropdownMenuItem asChild>
-                    <Button onClick={logout} size="sm" className="flex justify-start w-full rounded-[0.5rem] rounded-b-2xl">
-                        Cerrar Sesion
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar className="h-8 w-8 border p-1 hover:border-primary">
+                    <AvatarImage src="/react.svg" alt="user" />
+                    <AvatarFallback>{user?.username}</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem asChild>
+                    <Button
+                        size="sm"
+                        variant="ghost"
+                        className="mb-2 flex w-full justify-start rounded-[0.5rem]"
+                    >
+                        <Link to="/profile"> Perfil </Link>
                     </Button>
                 </DropdownMenuItem>
-                : <>
+                {isAuthenticated ? (
                     <DropdownMenuItem asChild>
-                        <Button size="sm" variant="ghost" className="flex mb-2 justify-start w-full rounded-[0.5rem]" >
-                            <Link to="/login"> Iniciar Sesion </Link>
+                        <Button
+                            onClick={logout}
+                            size="sm"
+                            className="flex w-full justify-start rounded-[0.5rem] rounded-b-2xl"
+                        >
+                            Cerrar Sesion
                         </Button>
                     </DropdownMenuItem>
-                    <DropdownMenuItem asChild>
-                        <Button size="sm" className="flex justify-start w-full rounded-[0.5rem] rounded-b-2xl">
-                            <Link to="/register"> Crear Cuenta </Link>
-                        </Button>
-                    </DropdownMenuItem>
-                </>
-            }
-        </DropdownMenuContent>
-    </DropdownMenu>
+                ) : (
+                    <>
+                        <DropdownMenuItem asChild>
+                            <Button
+                                size="sm"
+                                variant="ghost"
+                                className="mb-2 flex w-full justify-start rounded-[0.5rem]"
+                            >
+                                <Link to="/login"> Iniciar Sesion </Link>
+                            </Button>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Button
+                                size="sm"
+                                className="flex w-full justify-start rounded-[0.5rem] rounded-b-2xl"
+                            >
+                                <Link to="/register"> Crear Cuenta </Link>
+                            </Button>
+                        </DropdownMenuItem>
+                    </>
+                )}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 }
-
