931e3575be23ed68d016f4893fe4325381ba639a
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-02 23:54:58 -0400

    new utils fns

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,5 +1,6 @@
 import { FieldApi } from "@tanstack/react-form";
 import { type ClassValue, clsx } from "clsx";
+import { count } from "console";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,11 +9,39 @@ export function cn(...inputs: ClassValue[]) {
 
 export function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
     return (
-        <span className='text-white/50 text-sm'>
+        <span className="text-sm text-white/50">
             {field.state.meta.touchedErrors ? (
                 <span> {field.state.meta.touchedErrors} </span>
             ) : null}
-            {field.state.meta.isValidating ? 'Validating...' : null}
+            {field.state.meta.isValidating ? "Validating..." : null}
         </span>
-    )
+    );
+}
+
+export function mostFrequent(arr) {
+    const counts = {};
+
+    for (let num of arr)
+        counts[num] = (counts[num] || 0) + 1;
+
+    const maxCount = Math.max(...Object.values(counts));
+    const mostFrequent = Object
+        .keys(counts)
+        .find(key => counts[key] === maxCount);
+
+    return mostFrequent;
+}
+
+export const sleep = (delay: number) =>
+    new Promise((resolve) => setTimeout(resolve, delay))
+
+export function removeEmptyValues<T>(obj: T): T {
+    const newObj = { ...obj }
+
+    for (const key in newObj) {
+        if (newObj[key] === '')
+            delete newObj[key]
+    }
+
+    return newObj
 }
