73f466b317102feda33fff933130f4eb116546d6
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-02 23:56:23 -0400

    format: apply prettier

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,5 +1,7 @@
-import { useCartStore } from '@/store/cart'
-import { Button } from '@/components/ui/button'
+import { useCartStore } from "@/store/cart";
+import { Product } from "@/api/products";
+//shadcn
+import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -7,102 +9,173 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { DollarSignIcon, HeartIcon, MinusIcon, PlusIcon, ShoppingCartIcon, StoreIcon, TrashIcon, WalletIcon, XIcon } from 'lucide-react'
-import { Product } from '@/api/products'
+} from "@/components/ui/dropdown-menu";
+import {
+    HeartIcon,
+    PlusIcon,
+    ShoppingCartIcon,
+    TrashIcon,
+    WalletIcon,
+    XIcon,
+} from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { useAuth } from "@/context/auth";
+import { makeSale } from "@/api/sales";
+import { redirect } from "@tanstack/react-router";
+import { useCallback } from "react";
 
 export function FavoriteButton({ productId }: { productId: number }) {
-    return <Button size="icon" variant="ghost" onClick={() => { }} >
-        <HeartIcon className='h-4 w-4' />
-    </Button>
+    return (
+        <Button size="icon" variant="ghost" onClick={() => { }}>
+            <HeartIcon className="h-4 w-4" />
+        </Button>
+    );
 }
 
 export function AddToCartButton({ product }: { product: Product }) {
-    const handleAddToCart = useCartStore(s => s.add)
-    return <Button size="sm" onClick={() => handleAddToCart(product)} >
-        <ShoppingCartIcon className='mr-2 h-4 w-4' />
-        Añadir
-    </Button>
+    const handleAddToCart = useCartStore((s) => s.add);
+    return (
+        <Button size="sm" onClick={() => handleAddToCart(product)}>
+            <ShoppingCartIcon className="mr-2 h-4 w-4" />
+            Añadir
+        </Button>
+    );
 }
 
 export function DeleteFromCartButton({ productId }: { productId: number }) {
-    const remove = useCartStore(s => s.remove)
-    return <Button size="icon" variant="ghost" onClick={() => remove(productId)} >
-        <XIcon className='h-4 w-4' />
-    </Button>
+    const remove = useCartStore((s) => s.remove);
+    return (
+        <Button size="icon" variant="ghost" onClick={() => remove(productId)}>
+            <XIcon className="h-4 w-4" />
+        </Button>
+    );
 }
 
 export function IncreaseCountButton({ product }: { product: Product }) {
-    const add = useCartStore(s => s.add)
-    return <Button onClick={() => add(product)} size="icon" variant="outline" className='h-6 w-6 rounded-[8px]'>
-        <PlusIcon className='h-4 w-4' />
-    </Button>
+    const add = useCartStore((s) => s.add);
+    return (
+        <Button
+            onClick={() => add(product)}
+            size="icon"
+            variant="outline"
+            className="h-6 w-6 rounded-[8px]"
+        >
+            <PlusIcon className="h-4 w-4" />
+        </Button>
+    );
 }
 
 export function DecreaseCountButton({ productId }: { productId: number }) {
-    const remove = useCartStore(s => s.remove)
-    return <Button onClick={() => remove(productId)} size="icon" variant="outline" className='h-6 w-6 rounded-[8px]'>
-        <PlusIcon className='h-4 w-4' />
-    </Button>
+    const remove = useCartStore((s) => s.remove);
+    return (
+        <Button
+            onClick={() => remove(productId)}
+            size="icon"
+            variant="outline"
+            className="h-6 w-6 rounded-[8px]"
+        >
+            <PlusIcon className="h-4 w-4" />
+        </Button>
+    );
 }
 
 export function RemoveAllFromButton({ productId }: { productId: number }) {
-    const removeAllFrom = useCartStore(s => s.removeAllFrom)
-    return <Button onClick={() => removeAllFrom(productId)} size="icon" variant="ghost" className='h-6 w-6'>
-        <TrashIcon className='h-4 w-4' />
-    </Button>
+    const removeAllFrom = useCartStore((s) => s.removeAllFrom);
+    return (
+        <Button
+            onClick={() => removeAllFrom(productId)}
+            size="icon"
+            variant="ghost"
+            className="h-6 w-6"
+        >
+            <TrashIcon className="h-4 w-4" />
+        </Button>
+    );
 }
 
+const fallback = "/";
 
 export function CartDropdown() {
-    const cart = useCartStore(s => s.cart)
-    const count = useCartStore(s => s.count)
-    const total = count()
-    const totalPriceFn = useCartStore(s => s.totalPrice)
-    const totalPrice = totalPriceFn()
-    const removeAll = useCartStore(s => s.removeAll)
+    const cart = useCartStore((s) => s.cart);
+    const count = useCartStore((s) => s.count);
+    const total = count();
+    const totalPriceFn = useCartStore((s) => s.totalPrice);
+    const totalPrice = totalPriceFn();
+    const removeAll = useCartStore((s) => s.removeAll);
+    const { user } = useAuth();
 
-    return <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button size="icon" variant="ghost">
-                <ShoppingCartIcon className="w-4 h-4" />
-                {total > 0 && <span className="ml-1"> {total} </span>}
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuLabel>Tu Carrito</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {cart.map(item =>
-                <DropdownMenuItem key={item.id}>
-                    <div className='flex justify-between items-center gap-4'>
-                        <p className='w-20'> {item.name} </p>
-                        <div className='flex items-center gap-2'>
-                            <DecreaseCountButton productId={item.id} />
-                            {item.count}
-                            <IncreaseCountButton product={item} />
+    const { mutate } = useMutation({
+        mutationKey: ["sales"],
+        mutationFn: () => {
+            const products = cart.map(item => ({ id: item.id, count: item.count }))
+            return makeSale(user!.sub, products)
+        }
+    });
+
+    const handleMakeSale = useCallback(() => {
+        if (!user)
+            throw redirect({
+                to: "/login",
+                search: { redirect: fallback },
+            });
+
+        console.log("make sale", { user });
+
+        mutate();
+    }, [mutate, user]);
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button size="icon" variant="ghost">
+                    <ShoppingCartIcon className="h-4 w-4" />
+                    {total > 0 && <span className="ml-1"> {total} </span>}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Tu Carrito</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {cart.map((item) => (
+                    <DropdownMenuItem key={item.id}>
+                        <div className="flex items-center justify-between gap-4">
+                            <p className="w-20"> {item.name} </p>
+                            <div className="flex items-center gap-2">
+                                <DecreaseCountButton productId={item.id} />
+                                {item.count}
+                                <IncreaseCountButton product={item} />
+                            </div>
+                            <p className="font-semibold">${item.price}</p>
+                            <RemoveAllFromButton productId={item.id} />
                         </div>
-                        <p className='font-semibold'>${item.price}</p>
-                        <RemoveAllFromButton productId={item.id} />
+                    </DropdownMenuItem>
+                ))}
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel className="flex items-center justify-between">
+                    <p> Total </p>
+                    <p> $ {totalPrice} </p>
+                </DropdownMenuLabel>
+                <DropdownMenuItem asChild>
+                    <div className="flex w-full justify-between gap-2">
+                        <Button
+                            onClick={removeAll}
+                            size="sm"
+                            variant="outline"
+                            className="w-full"
+                        >
+                            <TrashIcon className="mr-2 h-4 w-4" />
+                            Limpiar
+                        </Button>
+                        <Button
+                            onClick={handleMakeSale}
+                            size="sm"
+                            className="w-full"
+                        >
+                            <WalletIcon className="mr-2 h-4 w-4" />
+                            Comprar
+                        </Button>
                     </div>
                 </DropdownMenuItem>
-            )}
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel className='flex items-center justify-between'>
-                <p> Total </p>
-                <p> $ {totalPrice} </p>
-            </DropdownMenuLabel>
-            <DropdownMenuItem>
-                <div className='flex justify-between gap-2 w-full' >
-                    <Button onClick={removeAll} size="sm" variant="outline" className='w-full' >
-                        <TrashIcon className='mr-2 h-4 w-4' />
-                        Limpiar
-                    </Button>
-                    <Button size="sm" className='w-full'>
-                        <WalletIcon className='mr-2 h-4 w-4' />
-                        Comprar
-                    </Button>
-                </div>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu >
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 }
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,93 +1,108 @@
-import { api } from "@/api"
-import { Role } from "@/api/users"
-import { createContext, useCallback, useContext, useState } from "react"
+import { api } from "@/api";
+import { Role } from "@/api/users";
+import { createContext, useCallback, useContext, useState } from "react";
 
 interface User {
-    sub: number
-    username: string
-    role: Role
-    iat: number
-    exp: number
+    sub: number;
+    username: string;
+    role: Role;
+    iat: number;
+    exp: number;
 }
 
 interface LoginCredentials {
-    username: string
-    password: string
+    username: string;
+    password: string;
 }
 
 interface RegisterCredentials extends LoginCredentials {
-    name: string
-    age: number
+    name: string;
+    age: number;
 }
 
 export interface AuthContext {
-    token: string | null
-    isAuthenticated: boolean
-    logout: () => Promise<void>
-    login: (credentials: LoginCredentials) => Promise<void>
-    register: (credentials: RegisterCredentials) => Promise<void>
-    user: User | null
+    token: string | null;
+    isAuthenticated: boolean;
+    logout: () => Promise<void>;
+    login: (credentials: LoginCredentials) => Promise<void>;
+    register: (credentials: RegisterCredentials) => Promise<void>;
+    user: User | null;
 }
 
-interface LoginRes { token: string }
+interface LoginRes {
+    token: string;
+}
 
-const AuthContext = createContext<AuthContext | null>(null)
+const AuthContext = createContext<AuthContext | null>(null);
 
-const key = 'auth.user'
+const key = "auth.user";
 
 function getStoredUser(): User | null {
-    const storedUser = localStorage.getItem(key)
-    if (storedUser) return JSON.parse(storedUser)
-    return null
+    const storedUser = localStorage.getItem(key);
+    if (storedUser) return JSON.parse(storedUser);
+    return null;
 }
 
-function setStoredUser(user: User | null) {
-    const serializedUser = JSON.stringify(user)
-    if (user) localStorage.setItem(key, serializedUser)
-    else localStorage.removeItem(key)
+function persistUser(user: User | null) {
+    if (user) {
+        const serializedUser = JSON.stringify(user);
+        localStorage.setItem(key, serializedUser);
+    } else localStorage.removeItem(key);
 }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [token, setToken] = useState<string | null>(null)
-    const [user, setUser] = useState<User | null>(getStoredUser())
-    const isAuthenticated = !!token
+    const [token, setToken] = useState<string | null>(null);
+    const [user, setUser] = useState<User | null>(getStoredUser());
+    const isAuthenticated = !!token;
 
     const logout = useCallback(async () => {
-        setToken(null)
-        setUser(null)
-        setStoredUser(null)
-    }, [])
+        setToken(null);
+        setUser(null);
+        persistUser(null);
+    }, []);
 
     const login = useCallback(async (credentials: LoginCredentials) => {
-        const { data } = await api.post<LoginRes>("/auth/login", credentials)
-        if (!data.token) throw new Error("Error while trying to login")
+        const { data } = await api.post<LoginRes>("/auth/login", credentials);
+        if (!data.token) throw new Error("Error while trying to login");
 
         const { data: userRes } = await api.get<User>("/auth/me", {
-            headers: { Authorization: `Bearer ${data.token}` }
-        })
-        if (!userRes) throw new Error("An Error happend while trying to fetch user's data")
+            headers: { Authorization: `Bearer ${data.token}` },
+        });
+        if (!userRes)
+            throw new Error(
+                "An Error happend while trying to fetch user's data",
+            );
 
-        setToken(data.token)
-        setUser(userRes)
-        setStoredUser(userRes)
-    }, [])
+        setToken(data.token);
+        setUser(userRes);
+        persistUser(userRes);
+    }, []);
 
-    const register = useCallback(async (credentials: RegisterCredentials) => {
-        const { data } = await api.post<LoginRes>("/auth/register", credentials)
-        if (!data) throw new Error("Error while trying to register")
+    const register = useCallback(
+        async (credentials: RegisterCredentials) => {
+            const { data } = await api.post<LoginRes>(
+                "/auth/register",
+                credentials,
+            );
+            if (!data) throw new Error("Error while trying to register");
 
-        await login(credentials)
-    }, [login])
+            await login(credentials);
+        },
+        [login],
+    );
 
-    return <AuthContext.Provider value={{ token, isAuthenticated, user, login, register, logout }}>
-        {children}
-    </AuthContext.Provider>
-
+    return (
+        <AuthContext.Provider
+            value={{ token, isAuthenticated, user, login, register, logout }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export function useAuth() {
-    const context = useContext(AuthContext)
-    if (!context) throw new Error('useAuth must be used within an AuthProvider')
-    return context
+    const context = useContext(AuthContext);
+    if (!context)
+        throw new Error("useAuth must be used within an AuthProvider");
+    return context;
 }
-
