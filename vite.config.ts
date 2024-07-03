916e4c7820f99fd89a47ba6623f28d5b3a9c3c19
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-02 23:47:41 -0400

    build: add vite image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import path from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), ViteImageOptimizer()],
   server: { port: 3000 },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
