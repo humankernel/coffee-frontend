515471bc547ee738e9834bde4c9908e2134d8ee3
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-04 01:15:16 -0400

    fix: add overflow to datatable

--- src/components/table/data-table.tsx
+++ src/components/table/data-table.tsx
@@ -130,7 +130,7 @@ export function DataTable<TData, TValue>({
                         </TableRow>
                     ))}
                 </TableHeader>
-                <TableBody>
+                <TableBody className="overflow-x-scroll">
                     {table.getRowModel().rows?.length ? (
                         table.getRowModel().rows.map((row) => (
                             <TableRow
