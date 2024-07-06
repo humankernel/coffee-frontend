5fe6952b2637724240d9688d6c4ed43201a72283
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-06 12:52:12 -0400

    refactor: improve password validator

--- src/components/forms/validators.tsx
+++ src/components/forms/validators.tsx
@@ -1,5 +1,5 @@
 import { Size, Temp } from "@/api/products";
-import { QsType } from "@/api/qs";
+import { CsType, Status } from "@/api/qs";
 import { Role } from "@/api/users";
 import { z } from "zod";
 
@@ -50,38 +50,17 @@ export const passwordValidators = {
     onChange: z
         .string()
         .min(8, "Minimo 8 caracteres")
-        .refine(
-            (pass) => {
-                const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
-                const containsLowercase = (ch: string) => /[a-z]/.test(ch);
-                const containsSpecialChar = (ch: string) =>
-                    /[`!@#$%^&*()_+\[\]{};':"\\|,.<>\/?~]/.test(ch);
-                let countOfUppercase = 0,
-                    countOfLowercase = 0,
-                    countOfNumbers = 0,
-                    countOfSpecialChar = 0;
-                for (let i = 0; i < pass.length; i++) {
-                    const ch = pass.charAt(i);
-                    if (!isNaN(+ch)) countOfNumbers++;
-                    else if (containsUppercase(ch)) countOfUppercase++;
-                    else if (containsLowercase(ch)) countOfLowercase++;
-                    else if (containsSpecialChar(ch)) countOfSpecialChar++;
-                }
-                return (
-                    countOfLowercase > 0 &&
-                    countOfUppercase > 0 &&
-                    countOfSpecialChar > 0 &&
-                    countOfNumbers > 0
-                );
-            },
-            {
-                message:
-                    "Debe contener: numeros, caracteres especiales, minusculas, Mayusculas",
-            },
-        )
+        .refine((value) => /[`!@#$%^&*()_+\[\]{};':"\\|,.<>\/?~]/.test(value),
+            { message: "Debe contener al menos 1 caracter especial" })
+        .refine((value) => /[a-z]/.test(value),
+            { message: "Debe contener al menos 1 letra minuscula" })
+        .refine((value) => /[A-Z]/.test(value),
+            { message: "Debe contener al menos 1 letra Mayuscula" })
+        .refine((value) => /.*[0-9].*/.test(value),
+            { message: "Debe contener al menos 1 numero" })
         .or(z.string().length(0)),
     onChangeAsyncDebounceMs: 500,
-};
+}
 
 export const roleValidators = {
     onChange: z
@@ -128,7 +107,14 @@ export const drinkValidators = {
 
 export const qsTypeValidators = {
     onChange: z
-        .nativeEnum(QsType)
+        .nativeEnum(CsType)
+        .or(z.string().length(0)),
+    onChangeAsyncDebounceMs: 500,
+};
+
+export const statusValidators = {
+    onChange: z
+        .nativeEnum(Status)
         .or(z.string().length(0)),
     onChangeAsyncDebounceMs: 500,
 };
