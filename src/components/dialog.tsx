974a4a3607956200d2250d7b1ad2198b2a9e976c
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-06 12:51:31 -0400

    rm unused code

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -5,26 +5,6 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
-
-function InsertDialog({ name, form }: { name: string, form: React.ReactNode }) {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button size="sm">
-                    <PlusIcon className="mr-2 h-4 w-4" /> Añadir
-                </Button>
-            </DialogTrigger>
-            <DialogContent className="p-4 sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Insertar {name}</DialogTitle>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">{form}</div>
-            </DialogContent>
-        </Dialog>
-    );
-}
 
 export function UpdateDialog({ title: title, children }: { title: string, children: React.ReactNode }) {
     return (
