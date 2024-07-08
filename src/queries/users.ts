e5b87ceda65eba79e54ab5ecd4293a4bacb228f0
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-08 03:21:32 -0400

    feat: new users queries

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -1,4 +1,11 @@
-import { deleteUserById, getUserById, getUsers } from "@/api/users";
+import {
+    User,
+    createUser,
+    deleteUserById,
+    getUserById,
+    getUsers,
+    updateUserById,
+} from "@/api/users";
 import {
     queryOptions,
     useMutation,
@@ -18,6 +25,41 @@ export const userOptions = (id: number) =>
         queryFn: () => getUserById(id),
         enabled: !!id,
     });
+
+export function useCreateUser() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: createUser,
+        onSuccess: (data) => {
+            toast.success(`Usuario ${data.name} correctamente creado`);
+            queryClient.invalidateQueries({ queryKey: ["users"] });
+        },
+        onError: (error) => {
+            toast.error("Error al crear el usuario");
+            if (error instanceof AxiosError)
+                toast.error(error.response?.data?.message);
+        },
+    });
+}
+
+export function useUpdateUser() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ id, user }: { id: number; user: Omit<User, "id"> }) =>
+            updateUserById(id, user),
+        onSuccess: (data) => {
+            toast.success(`Usuario ${data.name} correctamente actualizado`);
+            queryClient.invalidateQueries({ queryKey: ["users"] });
+        },
+        onError: (error) => {
+            toast.error("Error al actualizar el usuario");
+            if (error instanceof AxiosError)
+                toast.error(error.response?.data?.message);
+        },
+    });
+}
 
 export function useDeleteUser() {
     const queryClient = useQueryClient();
